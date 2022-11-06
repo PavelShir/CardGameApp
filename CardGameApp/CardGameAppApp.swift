c8c9217c3fa114eadd5a945c4df776e2f419879d
Pavel Shirokiy <pshirokiy@gmail.com>
2022-11-06 22:09:40 +0300

    Initial Commit

---
+++ CardGameApp/CardGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameAppApp.swift
+//  CardGameApp
+//
+//  Created by 19543442 on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
