9eb36fad070d2d23b6595c6a4f69695f2aefd34d
Pavel Shirokiy <pshirokiy@gmail.com>
2022-11-07 16:45:53 +0300

    done some logic

--- CardGameApp/ContentView.swift
+++ CardGameApp/ContentView.swift
@@ -11,17 +11,49 @@ struct ContentView: View {
     @State private var cardIsTaken = false
     
     private let buttonLabel = [
-        "Взять карту", "Взять вторую", "Взять третью", "Взять четвертую"
+        "Начать игру", "Взять карту", "Вторую", "Третью", "Четвертую", "Скинуть"
     ]
     @State private var index = 0
     
     var body: some View {
         
         VStack {
+            
+            Spacer()
+            
+            ZStack(alignment: .center) {
+               
+                switch index {
+                case 0, 1:
+                    Label("Игрок 1", systemImage: "person.fill")
+                        .frame(width: 250, height: 100)
+                        .font(.system(size: 26))
+                case 2:
+                    DiamondView()
+                        .transition(.move(edge: .top))
+                case 3:
+                    DiamondView()
+                    SpaidView()
+                        .transition(.move(edge: .top))
+                case 4:
+                    DiamondView()
+                    SpaidView()
+                    ClubView()
+                        .transition(.move(edge: .top))
+                default:
+                    DiamondView()
+                    SpaidView()
+                    ClubView()
+                    HeartView()
+                        .transition(.move(edge: .top))
+                }
+            }
+            Spacer()
+            
             Button(
                 action: {
                     buttonAction()
-                    if index < 3 {
+                    if index < 5 {
                         index += 1
                     } else {
                         index = 0
@@ -30,22 +62,6 @@ struct ContentView: View {
                     Text(buttonLabel[index])
                 })
             .font(.system(size: 26))
-            Spacer()
-            
-            ZStack(alignment: .center) {
-               
-                if cardIsTaken {
-                    
-                    DiamondView()
-                        .transition(.move(edge: .bottom))
-                }
-                
-                SpaidView()
-                    .offset(x: cardIsTaken ? 0 : -UIScreen.main.bounds.height)
-                ClubView()
-                HeartView()
-                
-            }
             Spacer()
         }
     }
@@ -56,6 +72,9 @@ struct ContentView: View {
         }
     }
     
+    private func takeCard(view: UIView) {
+        
+    }
     
 }
 
